e899e5b9c02fdcead488724d21755257475bc5e0
longkangjack <1030269471@qq.com>
2019-10-16 23:33:16 +0800

    更新练习题 有栈

--- 数据结构练习题/数据结构练习题/test.c
+++ 数据结构练习题/数据结构练习题/test.c
@@ -71,3 +71,41 @@ struct ListNode* reverseList(struct ListNode* head)
 	}
 	return head;
 }
+给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+
+有效字符串需满足：
+
+
+左括号必须用相同类型的右括号闭合。
+左括号必须以正确的顺序闭合。
+
+
+注意空字符串可被认为是有效字符串。
+
+//栈的例题
+bool isValid(char* s)
+{
+
+	int length = 0;//定义字符串长度
+	while (*(s + length))length++;//获取字符串长度
+	char* ptr = (char*)malloc(length / 2);//分配内存空间
+	memset(ptr, 0, length / 2);//初始化内存空间
+	int i, a = 0;
+	for (i = 0; i < length; i++)
+	{
+		if ((*(s + i) == '(') || (*(s + i) == '{') || (*(s + i) == '['))
+		{
+			a++;
+			*(ptr + a) = *(s + i);
+		}
+		//'('与')'的ASCII值差1，'['与']'，'{'与'}'的ASCII值差2
+		else if ((*(s + i) == (*(ptr + a) + 1)) || (*(s + i) == (*(ptr + a) + 2)))
+		{
+			a--;
+		}
+		else return 0;
+	}
+	if (a)
+		return 0;
+	return 1;
+}
