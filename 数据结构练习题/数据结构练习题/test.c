cfc570d470c28eae61a2f77726f3cd2d64b5930b
longkangjack <1030269471@qq.com>
2019-10-21 16:40:28 +0800

    更新练习题

--- 数据结构练习题/数据结构练习题/test.c
+++ 数据结构练习题/数据结构练习题/test.c
@@ -109,3 +109,158 @@ bool isValid(char* s)
 		return 0;
 	return 1;
 }
+使用栈实现队列的下列操作：
+
+
+push(x) --将一个元素放入队列的尾部。
+pop() --从队列首部移除元素。
+peek() --返回队列首部的元素。
+empty() --返回队列是否为空。
+#define DataType int
+#define MAXSIZE  100
+typedef struct
+{
+	DataType *stack_in;
+	DataType *stack_out;
+	int top_in;
+	int top_out;
+} MyQueue;
+/** Initialize your data structure here. */
+MyQueue* myQueueCreate() {
+	MyQueue *S = (MyQueue*)malloc(sizeof(MyQueue));
+	if (S == NULL)
+		return NULL;
+	S->stack_in = (int*)malloc(sizeof(DataType)*MAXSIZE);
+	S->stack_out = (int*)malloc(sizeof(DataType)*MAXSIZE);
+	S->top_in = -1;
+	S->top_out = -1;
+	return S;
+}
+/** Push element x to the back of queue. */
+void myQueuePush(MyQueue* obj, int x) {
+	obj->stack_in[++(obj->top_in)] = x;
+}
+/** Removes the element from in front of queue and returns that element. */
+int myQueuePop(MyQueue* obj) {
+	if (obj->top_out == -1)
+	{
+		while (obj->top_in != -1)
+		{
+			obj->stack_out[++(obj->top_out)] = obj->stack_in[(obj->top_in)--];
+		}
+	}
+	return obj->stack_out[(obj->top_out)--];
+}
+/** Get the front element. */
+int myQueuePeek(MyQueue* obj) {
+	if (obj->top_out != -1)
+	{
+		return obj->stack_out[(obj->top_out)];
+	}
+	else if (obj->top_in != -1)
+	{
+		return obj->stack_in[0];
+	}
+	else
+		return 0;
+}
+/** Returns whether the queue is empty. */
+bool myQueueEmpty(MyQueue* obj) {
+	return (obj->top_in == -1 && obj->top_out == -1) ? true : false;
+}
+
+void myQueueFree(MyQueue* obj) {
+	free(obj);
+}
+/**
+* Your MyQueue struct will be instantiated and called as such:
+* MyQueue* obj = myQueueCreate();
+* myQueuePush(obj, x);
+
+* int param_2 = myQueuePop(obj);
+
+* int param_3 = myQueuePeek(obj);
+
+* bool param_4 = myQueueEmpty(obj);
+
+* myQueueFree(obj);
+*/
+//设计你的循环队列实现。 循环队列是一种线性数据结构，其操作表现基于 FIFO（先进先出）原则并且队尾被连接在队首之后以形成一个循环。它也被称为“环形缓冲器”。
+
+//循环队列的一个好处是我们可以利用这个队列之前用过的空间。在一个普通队列里，一旦一个队列满了，我们就不能插入下一个元素，即使在队列前面仍有空间。但是使用循环队列，我们能使用这些空间去存储新的值。//
+typedef struct {
+	int *que;
+	int front;
+	int rear;
+	int length;
+} MyCircularQueue;
+
+/** Initialize your data structure here. Set the size of the queue to be k. */
+
+MyCircularQueue* myCircularQueueCreate(int k) {
+	MyCircularQueue *q1;
+	q1 = malloc(sizeof(MyCircularQueue));
+	q1->que = malloc(sizeof(int)* (k + 1));
+	q1->front = q1->rear = 0;
+	q1->length = k + 1;
+	return q1;
+}
+
+/** Insert an element into the circular queue. Return true if the operation is successful. */
+bool myCircularQueueEnQueue(MyCircularQueue* obj, int value) {
+	if ((obj->rear + 1) % (obj->length) == obj->front)
+		return false;
+	else
+	{
+		obj->rear = (obj->rear + 1) % (obj->length);
+		obj->que[obj->rear] = value;
+		return true;
+	}
+}
+
+/** Delete an element from the circular queue. Return true if the operation is successful. */
+bool myCircularQueueDeQueue(MyCircularQueue* obj) {
+	if (obj->rear == obj->front)
+		return false;
+	else
+	{
+		obj->front = (obj->front + 1) % (obj->length);
+		return true;
+	}
+}
+
+/** Get the front item from the queue. */
+int myCircularQueueFront(MyCircularQueue* obj) {
+	if (obj->rear == obj->front)
+		return -1;
+	return obj->que[(obj->front + 1) % obj->length];
+
+}
+
+/** Get the last item from the queue. */
+int myCircularQueueRear(MyCircularQueue* obj) {
+	if (obj->rear == obj->front)
+		return -1;
+	return obj->que[obj->rear];
+}
+
+/** Checks whether the circular queue is empty or not. */
+bool myCircularQueueIsEmpty(MyCircularQueue* obj) {
+	if (obj->rear == obj->front)
+		return true;
+	return false;
+}
+
+/** Checks whether the circular queue is full or not. */
+bool myCircularQueueIsFull(MyCircularQueue* obj) {
+	if ((obj->rear + 1) % (obj->length) == obj->front)
+		return true;
+	return false;
+}
+
+void myCircularQueueFree(MyCircularQueue* obj) {
+	int *tmp;
+	tmp = obj->que;
+	obj->que = NULL;
+	free(tmp);
+}
